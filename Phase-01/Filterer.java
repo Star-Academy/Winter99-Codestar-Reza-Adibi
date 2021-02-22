8edf2d48b0a830fa92b2e8dc1267d061a1e11cd8
Reza ADB <adbrezaooo@gmail.com>
2021-02-23 00:00:01 +0330

    Phase-01->Filterer.java: OrWords() function added.

--- Phase-01/Filterer.java
+++ Phase-01/Filterer.java
@@ -3,11 +3,11 @@ import java.util.HashSet;
 
 public class Filterer {
     /**
-     * get inverted index( "words => docs" database ) and initial set and list of
-     * words, then use and to find docs that are included in initial set and docs
-     * list of each word.
+     * get inverted index( "words => docIDs" database ) and initial set and list of
+     * words, then use "and" to find docIDs that are included in initial set and
+     * docIDs list of each word.
      * 
-     * @param invertedIndex a "word-doc" database.
+     * @param invertedIndex a "word-docID" database.
      * @param initialSet    set of docIDs( String ) or null.
      * @param words         list of words( String ).
      * @return set of docIDs( String ) or null( error ).
@@ -21,19 +21,47 @@ public class Filterer {
         for (String word : words) {
             if (!invertedIndex.containsWord(word))
                 return new HashSet<>();
-            HashSet<String> wordDocs = new HashSet<>(invertedIndex.SearchForWord(word).keySet());
-            System.out.println("test");
+            HashSet<String> wordDocIDs = new HashSet<>(invertedIndex.SearchForWord(word).keySet());
             if (initialSet == null) {
-                initialSet = wordDocs;
+                initialSet = wordDocIDs;
             } else {
                 HashSet<String> initialSetTemp = new HashSet<String>();
                 initialSetTemp.addAll(initialSet);
-                for (String doc : initialSetTemp) {
-                    if (!wordDocs.contains(doc))
-                        initialSet.remove(doc);
+                for (String docID : initialSetTemp) {
+                    if (!wordDocIDs.contains(docID))
+                        initialSet.remove(docID);
                 }
                 if (initialSet.isEmpty())
                     return new HashSet<>();
+            }
+        }
+        return initialSet;
+    }
+
+    /**
+     * get "inverted index( "words => docIDs" database )" and "initial set" and
+     * "list of words", then add all docIDs of each word to "initial set" and return
+     * it.
+     * 
+     * @param invertedIndex a "word-docID" database.
+     * @param initialSet    set of docIDs( String ) or null.
+     * @param words         list of words( String ).
+     * @return set of docIDs( String ) or null( error ).
+     */
+    public static HashSet<String> OrWords(InvertedIndex invertedIndex, HashSet<String> initialSet,
+            ArrayList<String> words) {
+        if (words.isEmpty())
+            return null;
+        if (invertedIndex.IsEmpty())
+            return null;
+        for (String word : words) {
+            if (!invertedIndex.containsWord(word))
+                continue;
+            HashSet<String> wordDocs = new HashSet<>(invertedIndex.SearchForWord(word).keySet());
+            if (initialSet == null) {
+                initialSet = wordDocs;
+            } else {
+                initialSet.addAll(wordDocs);
             }
         }
         return initialSet;
