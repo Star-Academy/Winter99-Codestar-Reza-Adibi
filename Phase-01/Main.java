bcede1d60301353bd2587eb798b46b4d55cfbd39
Reza ADB <adbrezaooo@gmail.com>
2021-02-23 19:23:28 +0330

    Phase-01->Main.java: Filterer.NotWords() tested.

--- Phase-01/Main.java
+++ Phase-01/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
 
 public class Main {
@@ -11,9 +12,10 @@ public class Main {
         // System.out.println(invertedIndex.invertedIndexMap);
         ArrayList words = new ArrayList<String>();
         words.add("is");
-        words.add("file");
-        words.add("document");
-        // System.out.println(Filterer.AndWords(invertedIndex, null, words));
-        System.out.println(Filterer.OrWords(invertedIndex, null, words));
+//        words.add("file");
+//        words.add("document");
+//         System.out.println(Filterer.AndWords(invertedIndex, null, words));
+//        System.out.println(Filterer.OrWords(invertedIndex, null, words));
+        System.out.println(Filterer.NotWords(invertedIndex, (HashSet<String>) invertedIndex.GetAllDocIDs(), words));
     }
 }
