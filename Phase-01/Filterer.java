482f654fd3c31a98c4b272d25ceb4f894a533ec5
Reza ADB <adbrezaooo@gmail.com>
2021-02-23 19:22:22 +0330

    Phase-01->Filterer.java: NotWords() function added and other function a little optimized.

--- Phase-01/Filterer.java
+++ Phase-01/Filterer.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -13,9 +14,9 @@ public class Filterer {
      * @return set of docIDs( String ) or null( error ).
      */
     public static HashSet<String> AndWords(InvertedIndex invertedIndex, HashSet<String> initialSet, ArrayList<String> words) {
-        if (words.isEmpty())
+        if (words == null || words.isEmpty())
             return null;
-        if (invertedIndex.IsEmpty())
+        if (invertedIndex == null || invertedIndex.IsEmpty())
             return null;
         /* start with the word that has least docIDs */
         String startingWord = words.get(0);
@@ -74,9 +75,9 @@ public class Filterer {
      * @return set of docIDs( String ) or null( error ).
      */
     public static HashSet<String> OrWords(InvertedIndex invertedIndex, HashSet<String> initialSet, ArrayList<String> words) {
-        if (words.isEmpty())
+        if (words == null || words.isEmpty())
             return null;
-        if (invertedIndex.IsEmpty())
+        if (invertedIndex == null || invertedIndex.IsEmpty())
             return null;
         for (String word : words) {
             if (!invertedIndex.containsWord(word))
@@ -86,6 +87,47 @@ public class Filterer {
                 initialSet = wordDocs;
             } else {
                 initialSet.addAll(wordDocs);
+            }
+        }
+        return initialSet;
+    }
+
+    /**
+     * get "inverted index( "words => docIDs" database )" and "initial set" and
+     * "list of words", then remove all docIDs of each word from "initial set" and return
+     * it.
+     *
+     * @param invertedIndex a "word-docID" database.
+     * @param initialSet    set of docIDs( String ).
+     * @param words         list of words( String ).
+     * @return set of docIDs( String ) or null( error ).
+     */
+    public static HashSet<String> NotWords(InvertedIndex invertedIndex, HashSet<String> initialSet, ArrayList<String> words) {
+        if (words == null || words.isEmpty())
+            return null;
+        if (invertedIndex == null || invertedIndex.IsEmpty())
+            return null;
+        if (initialSet == null || initialSet.isEmpty())
+            return null;
+        /* add docID sets of words to one HashSet. */
+        HashSet<String> wordsDocIDs = new HashSet<String>();
+        for (String word : words) {
+            if (invertedIndex.containsWord(word)) {
+                wordsDocIDs.addAll(invertedIndex.SearchForWord(word).keySet());
+            }
+        }
+        /* check which one is a smaller set, initialSet or wordsDocIDs then remove common items by looping trough smaller set. */
+        if (initialSet.size() > wordsDocIDs.size()) {
+            for (String docID : wordsDocIDs) {
+                initialSet.remove(docID);
+            }
+        } else {
+            HashSet<String> initialSetTemp = new HashSet<>();
+            initialSetTemp.addAll(initialSet);
+            for (String docID : initialSetTemp
+            ) {
+                if (wordsDocIDs.contains(docID))
+                    initialSet.remove(docID);
             }
         }
         return initialSet;
