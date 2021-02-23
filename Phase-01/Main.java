8da8b0dadc3eb3724d584a486b906772baae2527
Reza ADB <adbrezaooo@gmail.com>
2021-02-23 23:24:16 +0330

    Phase-02->Main.java: GetResult() and GetFiltersFromUser() functions added.

--- Phase-01/Main.java
+++ Phase-01/Main.java
@@ -1,21 +1,75 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        Main This = new Main();
         TextFileReader reader = new TextFileReader();
-        Map<String, String> result = reader.readAllFileInFolder("data");
+        Map<String, String> result = reader.readAllFileInFolder("EnglishData");
         InvertedIndex invertedIndex = new InvertedIndex();
         invertedIndex.AddDocumets(result);
-        // System.out.println(result);
-        // System.out.println(invertedIndex.invertedIndexMap);
-        ArrayList words = new ArrayList<String>();
-        words.add("is");
-//        words.add("file");
-//        words.add("document");
-//         System.out.println(Filterer.AndWords(invertedIndex, null, words));
-//        System.out.println(Filterer.OrWords(invertedIndex, null, words));
-        System.out.println(Filterer.NotWords(invertedIndex, (HashSet<String>) invertedIndex.GetAllDocIDs(), words));
+        Map<String, ArrayList<String>> filters = This.GetFiltersFromUser();
+        System.out.println(This.GetResult(invertedIndex, filters));
+    }
+
+    /**
+     * get filters and create result( set of docIDs ).
+     *
+     * @param invertedIndex our data base of word-docIDs.
+     * @param filters       "and", "or" & "not" filters.
+     * @return set of docIDs that satisfy filters.
+     */
+    private Set GetResult(InvertedIndex invertedIndex, Map<String, ArrayList<String>> filters) {
+        HashSet<String> andResult = null;
+        HashSet<String> orResult = null;
+        if (!filters.get("or").isEmpty()) {
+            orResult = Filterer.OrWords(invertedIndex, new HashSet<>(), filters.get("or"));
+        }
+        if (!filters.get("and").isEmpty()) {
+            andResult = Filterer.AndWords(invertedIndex, orResult, filters.get("and"));
+        }
+        if (filters.get("not").isEmpty()) {
+            return filters.get("and").isEmpty() ? orResult : andResult;
+        }
+        if (!filters.get("or").isEmpty() && filters.get("and").isEmpty()) {
+            return Filterer.NotWords(invertedIndex, orResult, filters.get("not"));
+        } else if (!filters.get("and").isEmpty()) {
+            return Filterer.NotWords(invertedIndex, andResult, filters.get("not"));
+        } else {
+            return Filterer.NotWords(invertedIndex, invertedIndex.GetAllDocIDs(), filters.get("not"));
+        }
+    }
+
+    /**
+     * read user input and divide them in three lists of filters( "and", "or" & "not" ).
+     *
+     * @return filters Map( ["and", "or", "not"] => ArrayList of words ).
+     */
+    private Map<String, ArrayList<String>> GetFiltersFromUser() {
+        Map<String, ArrayList<String>> words = new HashMap<String, ArrayList<String>>();
+        words.put("and", new ArrayList<String>());
+        words.put("or", new ArrayList<String>());
+        words.put("not", new ArrayList<String>());
+        System.out.println("write your key words:");
+        String userInput = scanner.nextLine();
+        String[] rawWords = userInput.toLowerCase().split("[, .]");
+        for (String rawWord : rawWords) {
+            switch (rawWord.charAt(0)) {
+                case '+': {
+                    words.get("or").add(rawWord.substring(1));
+                    break;
+                }
+                case '-': {
+                    words.get("not").add(rawWord.substring(1));
+                    break;
+                }
+                default: {
+                    words.get("and").add(rawWord);
+                    break;
+                }
+            }
+        }
+        return words;
     }
 }
