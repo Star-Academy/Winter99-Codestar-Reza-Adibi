13e8a3828391e21812a9c509f4f4beb284105998
Reza ADB <adbrezaooo@gmail.com>
2021-02-24 20:09:46 +0330

    Phase-02->Main.java->GetResult(): moved to Filterer,java.

--- Phase-01/Main.java
+++ Phase-01/Main.java
@@ -3,47 +3,14 @@ import java.util.*;
 public class Main {
     Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Main This = new Main();
         TextFileReader reader = new TextFileReader();
         Map<String, String> result = reader.readAllFileInFolder("EnglishData");
         InvertedIndex invertedIndex = new InvertedIndex();
         invertedIndex.AddDocuments(result);
         Map<String, ArrayList<String>> filters = This.GetFiltersFromUser();
-        System.out.println(This.GetResult(invertedIndex, filters));
-    }
-
-    /**
-     * get filters and create result( set of docIDs ).
-     *
-     * @param invertedIndex our data base of word-docIDs.
-     * @param filters       "and", "or" & "not" filters.
-     * @return set of docIDs that satisfy filters.
-     */
-    private HashSet<String> GetResult(InvertedIndex invertedIndex, Map<String, ArrayList<String>> filters) {
-        HashSet<String> andResult = null;
-        HashSet<String> orResult = null;
-        try {
-            if (!filters.get("or").isEmpty()) {
-                orResult = Filterer.OrWords(invertedIndex, new HashSet<>(), filters.get("or"));
-            }
-            if (!filters.get("and").isEmpty()) {
-                andResult = Filterer.AndWords(invertedIndex, orResult, filters.get("and"));
-            }
-            if (filters.get("not").isEmpty()) {
-                return filters.get("and").isEmpty() ? orResult : andResult;
-            }
-            if (!filters.get("or").isEmpty() && filters.get("and").isEmpty()) {
-                return Filterer.NotWords(invertedIndex, orResult, filters.get("not"));
-            } else if (!filters.get("and").isEmpty()) {
-                return Filterer.NotWords(invertedIndex, andResult, filters.get("not"));
-            } else {
-                return Filterer.NotWords(invertedIndex, invertedIndex.GetAllDocIDs(), filters.get("not"));
-            }
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-            return new HashSet<>();
-        }
+        System.out.println(Filterer.GetResult(invertedIndex, filters));
     }
 
     /**
