c05485dedeb1e2aed95cb2bb4c3cb562974319bc
Reza ADB <adbrezaooo@gmail.com>
2021-03-05 20:57:26 +0330

    (Phase-04) Project-02: DataWriter Interface and ConsoleUI class added

---
+++ Project-02/ConsoleUI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+
+namespace Project_02 {
+    class ConsoleUI : DataWriter {
+        public void DisplayObjects(IEnumerable objects) {
+            foreach (object ob in objects) {
+                Console.WriteLine(ob.ToString());
+            }
+        }
+    }
+}
---
+++ Project-02/DataWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace Project_02 {
+    interface DataWriter {
+        /// <summary>
+        /// show given IEnumarable of objects.
+        /// </summary>
+        /// <param name="objects">
+        /// IEnumarable of objects to be displayed.
+        /// </param>
+        public void DisplayObjects(IEnumerable objects);
+    }
+}
