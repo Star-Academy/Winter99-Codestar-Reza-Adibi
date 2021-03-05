74c42322142dd1e8649ac12c99f07611af1b04db
Reza ADB <adbrezaooo@gmail.com>
2021-03-05 14:18:44 +0330

    (Phase-03) Project-01->src->main->java->Main.java: now variables in main() are final.

--- Project-01/src/main/java/Main.java
+++ Project-01/src/main/java/Main.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Map<String, String> result = TextFileReader.readAllFileInFolder("EnglishData");
-        InvertedIndex invertedIndex = new InvertedIndex();
+        final Map<String, String> result = TextFileReader.readAllFileInFolder("EnglishData");
+        final InvertedIndex invertedIndex = new InvertedIndex();
         invertedIndex.addDocuments(result);
-        UserInterface ui = new Console(new Scanner(System.in));
-        Map<String, ArrayList<String>> filters = ui.getUserInputFilters();
-        Filterer searchEngine = new Filterer(invertedIndex);
+        final UserInterface ui = new Console(new Scanner(System.in));
+        final Map<String, ArrayList<String>> filters = ui.getUserInputFilters();
+        final Filterer searchEngine = new Filterer(invertedIndex);
         System.out.println(searchEngine.filterDocIDs(filters));
     }
 
