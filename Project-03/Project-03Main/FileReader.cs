0fb28f7bdb29a24b4f79f26c7cfd1a0b9e9ad5ea
Reza ADB <adbrezaooo@gmail.com>
2021-03-08 23:17:59 +0330

    (Phase-05) Project-03->Project-03Main: FileReader added and test passed.

---
+++ Project-03/Project-03Main/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Project_03 {
+    public class FileReader {
+        private readonly string directoryPath;
+        public FileReader(string directoryPath) {
+            this.directoryPath = directoryPath;
+        }
+        public string GetRawData() {
+            var stringData = "";
+            var pathes = Directory.GetFiles(directoryPath);
+            foreach (string path in pathes)
+                stringData += File.ReadAllText(path) + '\n';
+            stringData = stringData.Remove(stringData.Length - 1);
+            return stringData;
+        }
+    }
+}
