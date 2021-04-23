1520e4845ad0ca59836311ef3e88118f226fc276
Reza Adibi <adbrezaooo@gmail.com>
2021-04-23 21:40:30 +0430

    (Phase-11) Project-09: Created.

---
+++ Project-09( Api )/Libraries/ElasticResponseValidator.cs
@@ -0,0 +1,23 @@
+﻿using Nest;
+using System;
+using System.Linq;
+
+namespace Libraries {
+    public static class ElasticResponseValidator {
+        public static T Validate<T>(this T response) where T : IResponse {
+            if (response == null)
+                throw new Exception("Unknown Error!");
+            if (!response.IsValid) {
+                if (response.OriginalException != null)
+                    throw response.OriginalException;
+                if (response.ServerError != null)
+                    throw new Exception(
+                        "Status: " + response.ServerError.Status +
+                        "\n Message: " + response.ServerError?.Error?.RootCause?.FirstOrDefault()?.Reason
+                    );
+                throw new Exception("Unknown Error!");
+            }
+            return response;
+        }
+    }
+}
---
+++ Project-09( Api )/Libraries/IQueryBuilder.cs
@@ -0,0 +1,44 @@
+﻿using Nest;
+using System.Collections.Generic;
+
+namespace Libraries {
+    public interface IQueryBuilder {
+
+        /// <summary>
+        /// Add given filter to list of filters.
+        /// </summary>
+        /// <param name="filter">A filter to be added to filters list.</param>
+        public void AddFilter(string filter);
+
+        /// <summary>
+        /// Add given filters to list of filters.
+        /// </summary>
+        /// <param name="filters">List of filters to be added to filters list.</param>
+        public void AddFilters(IEnumerable<string> filters);
+
+        /// <summary>
+        /// Remove given filter from list of filters.
+        /// </summary>
+        /// <param name="filter">A filter to be removed from filters list.</param>
+        public void RemoveFilter(string filter);
+
+        /// <summary>
+        /// Remove given filters from list of filters.
+        /// </summary>
+        /// <param name="filters">List of filters to be removed from filters list.</param>
+        public void RemoveFilters(IEnumerable<string> filters);
+
+        /// <summary>
+        /// Check if filters list contans given filter. 
+        /// </summary>
+        /// <param name="filter">The filter that we want to know if filters list contans it.</param>
+        /// <returns>If filters list contain given filter "true", otherwise "false".</returns>
+        public bool ContainsFilter(string filter);
+
+        /// <summary>
+        /// Generate list of NEST.QueryContainers using filters of this class.
+        /// </summary>
+        /// <returns>List of NEST.QueryContainers to use in nest bool query.</returns>
+        public IEnumerable<QueryContainer> GetQuery(string fieldName);
+    }
+}
